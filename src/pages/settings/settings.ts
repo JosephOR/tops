6d82bb9743862617ec2d1ce1af1187939efb5b04
Josephoreilly <josephoreilly29@hotmail.com>
2018-09-21 10:12:24 +0100

    elements added to details page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -6,12 +6,36 @@ import { NavController } from 'ionic-angular';
 	templateUrl: 'settings.html'
 })
 export class SettingsPage {
-sets:any[] = [];
+	games:any[] = [];
+	sets:any[] = [];
+	legs:any[] = [];
+	selectedGame:any;
+	selectedSet:any;
+	selectedLeg:any;
   	constructor(public navCtrl: NavController) {
-  		this.sets = ['one', 'two', 'three']
+  		this.games = [ { name: '301' }, { name: '501' }];
+  		this.selectedGame = this.games[0];
+  		this.sets = [ { name: '1' }, { name: '2' }, { name: '3' }];
+  		this.selectedSet = this.sets[0];
+  		this.legs = [ { name: '1' }, { name: '2' }, { name: '3' }];
+  		this.selectedLeg = this.legs[0];
   	}
-  	isActive(item) {
-  		console.log(item);
-  		return 'page-settings__active-set'
+  	isActiveSet(set) {
+  		return set.name === this.selectedSet.name ? 'page-settings__active-option' : 'page-settings__not-active-option';
+  	}
+  	setActiveSet(set) {
+  		this.selectedSet = set;
+  	}
+  	isActiveLeg(leg) {
+  		return leg.name === this.selectedLeg.name ? 'page-settings__active-option' : 'page-settings__not-active-option';
+  	}
+  	setActiveLeg(leg) {
+  		this.selectedLeg = leg;
+  	}
+  	isActiveGame(game) {
+  		return game.name === this.selectedGame.name ? 'page-settings__active-option' : 'page-settings__not-active-option';
+  	}
+  	setActiveGame(game) {
+  		this.selectedGame = game;
   	}
 }
